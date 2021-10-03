342181031c863e8c09681a9d56f00954ad07f4f0
Ale Dili <ale.diliberti@gmail.com>
2021-10-03 18:50:17 +0200

    Add checkSovrapposizioni

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,24 @@ public class Main {
         return obj;
     }
 
+    public boolean checkSovrapposizioni(Appuntamento app1, Appuntamento app2){
+        boolean sovrapposti = false;
+        if (app1.getGiorno() == app2.getGiorno()){
+            if (app1.getOraInizio()==app2.getOraInizio()){
+                sovrapposti = true;
+            }else if (app1.getOraInizio()<app2.getOraInizio()){ //se app1 inizia prima di app2
+                if(app1.getOraFine()>app2.getOraInizio()){
+                    sovrapposti = true;
+                }
+            }else {
+                if(app2.getOraFine()>app1.getOraInizio()){
+                    sovrapposti = true;
+                }
+            }
+        }
+        return sovrapposti;
+    }
+
     public static void main(String[] args) {
 
     }
