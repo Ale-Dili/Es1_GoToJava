eb2d09fab7557bcad317c51a9b43d860ebfa3937
Ale Dili <ale.diliberti@gmail.com>
2021-10-04 21:19:19 +0200

    addAppuntamento and main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import java.util.*;
 public class Main {
     //[0] Appuntamento
     //[1] boolean
+    private static ArrayList<Appuntamento> agenda = new ArrayList<Appuntamento>();
+
     public static Object[] newAppuntamento(int gg,int  h1,int h2){
         Object[] obj = new Object[2];
         obj[0]=new Appuntamento(0,0,0);
@@ -34,6 +37,16 @@ public class Main {
         return sovrapposti;
     }
 
+    public static boolean addAppuntamento(Appuntamento app){
+        for (int i = 0; i < agenda.size(); i++) {
+            if (checkSovrapposizioni(app, agenda.get(i))){
+                return false;
+            }
+        }
+        agenda.add(app);
+        return true;
+    }
+
 
 
 
@@ -49,6 +62,35 @@ public class Main {
             System.out.println("Yup");
         }
         */
+        Scanner scanner = new Scanner(System.in);
+        int data,oraInizio,oraFine;
+        Object[] obj;
+        System.out.println("PREMI CTRL+D PER TERMINARE");
+        boolean continua = true;
+        while (continua) {
+            try {
+
+                System.out.println("Inserisci la data");
+                data = scanner.nextInt();
+                System.out.println("Inserisci l'ora d'inizio'");
+                oraInizio = scanner.nextInt();
+                System.out.println("Inserisci l'ora di fine'");
+                oraFine = scanner.nextInt();
+                obj = newAppuntamento(data, oraInizio, oraFine);
+                if ((boolean) obj[1]) {
+                    if (addAppuntamento((Appuntamento) obj[0])) {
+                        System.out.println("Appuntamento aggiunto");
+                    } else {
+                        System.out.println("Appuntamento sovrapposto");
+                    }
+                } else {
+                    System.out.println("I dati inseriti non sono corretti");
+                }
+            } catch (Exception e) {
+                continua = false;
+            }
+        }
+        System.out.println(agenda.toString());
 
     }
 }
